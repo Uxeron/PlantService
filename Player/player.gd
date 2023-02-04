76992c175a8c46f2dc1aace2a66aa9b7f17a45fe
Uxeron <vgabrielius123@gmail.com>
2023-02-04 16:54:14 +0200

    Added player animation

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,11 @@
 extends RigidBody2D
 class_name PhysicalCharacter
 
-@export var walking_speed: float = 30.0
+@export var walking_speed: float = 100.0
+
+@export var animation_speed: float = 0.2
+var animation_timer: Timer
+@onready var sprite: Sprite2D = $Sprite2D
 
 var accumulated_rotation: float = 0.0
 var accumulated_movement: Vector2 = Vector2.ZERO
@@ -12,6 +16,27 @@ var movement_direction_map: Dictionary = {
 	"move_right": Vector2.RIGHT,
 	"move_back": Vector2.DOWN,
 }
+
+func animate() -> void:
+	if sprite.frame + 1 >= sprite.vframes * sprite.hframes:
+		sprite.frame = 0
+	else:
+		sprite.frame += 1
+
+func _ready() -> void:
+	animation_timer = Timer.new()
+	animation_timer.one_shot = true
+	animation_timer.timeout.connect(animate)
+	add_child(animation_timer)
+
+func _process(delta: float) -> void:
+	if accumulated_movement != Vector2.ZERO:
+		if animation_timer.is_stopped():
+			animation_timer.start(animation_speed)
+	else:
+		if not animation_timer.is_stopped():
+			animation_timer.stop()
+			sprite.frame = 0
 
 func _physics_process(delta: float) -> void:
 	for direction in movement_direction_map.keys():
